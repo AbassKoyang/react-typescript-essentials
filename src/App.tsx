d8b2ba29ae4b507aab78fba66fab4abee4758956
Abass Koyang <abasskoyang12345@gmail.com>
2025-01-06 14:46:46 +0100

    completed TS challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Component from './starter/04-events';
+import Component from './starter/05-challenge';
 function App() {
   return (
     <main>
-      <Component />
+      <Component type="advanced" name="Abass" email="abass@gmail.com" />
+      <Component type="basic" name="Abass"/>
     </main>
   );
 }
--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,8 +1,13 @@
-function Component() {
+type ComponentProps = {
+  type: 'advanced' | 'basic';
+  name: string;
+  email?: string;
+}
+function Component({type, name, email} : ComponentProps) {
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
+    <div className={`alert ${type === 'advanced' ? 'alert-danger' : 'alert-success'}`}>
+      <h3>User: {name}</h3>
+      {email && <h3>Email: {email}</h3>}
     </div>
   );
 }
