bceb79b849479ecd995236f063820b54ceff555b
Abass Koyang <abasskoyang12345@gmail.com>
2025-01-06 15:06:05 +0100

    refactored challenge code

--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,14 +1,26 @@
-type ComponentProps = {
-  type: 'advanced' | 'basic';
+type BasicComponentProps = {
+  type: 'basic';
   name: string;
   email?: string;
 }
-function Component({type, name, email} : ComponentProps) {
-  return (
-    <div className={`alert ${type === 'advanced' ? 'alert-danger' : 'alert-success'}`}>
+type AdvancedComponentProps = {
+  type: 'advanced';
+  name: string;
+  email?: string;
+}
+type ComponentProps = BasicComponentProps | AdvancedComponentProps
+function Component(props: ComponentProps) {
+  const {type, name} = props;
+  if(type === 'basic'){
+    return (<div className='alert alert-success'>
       <h3>User: {name}</h3>
-      {email && <h3>Email: {email}</h3>}
+    </div>)
+  };
+  return (
+    <div className='alert alert-danger'>
+      <h3>User: {name}</h3>
+      <h3>Email: {props.email}</h3>
     </div>
-  );
+  )
 }
 export default Component;
